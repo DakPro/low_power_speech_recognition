45f62dd6ce2c0c274197cc597b53d967fa45c4b0
dk729@cam.ac.uk <kvit2006@gmail.com>
2025-08-07 11:43:16 +0100

    Code cleanup

--- transcription_from_mic.py
+++ transcription_from_mic.py
@@ -1,9 +1,9 @@
 import argparse
-import moonshine.live_transcription
+import moonshine.src.live_transcription as live_transcription
 
 SUPPORTED_MODELS = ["moonshine"]
 MODEL = "moonshine"
-DEFAULT_MODEL_SIZES = {"moonshine":"moonshine/base"}
+DEFAULT_MODEL_SIZES = {"moonshine": "moonshine/base"}
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -19,5 +19,5 @@ if __name__ == '__main__':
     args = parser.parse_args()
     model = args.model
 
-    if(model=='moonshine'):
-        moonshine.live_transcription.main(DEFAULT_MODEL_SIZES[model], include_captions=True)
+    if model == 'moonshine':
+        live_transcription.main(DEFAULT_MODEL_SIZES[model], include_captions=True)

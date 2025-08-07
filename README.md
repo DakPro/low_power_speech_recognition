ba7430b0fc1e7d935dde6eb2e99e81187963a69a
dk729@cam.ac.uk <kvit2006@gmail.com>
2025-08-07 11:20:43 +0100

    Live transcription

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Speech recognition models on low-power devices
+
 This repository is storage for code used in my summer research project
 
+## Real-time transcription from mic
+
+To test real-time transcription from mic: <code>python3 
+transcription_from_mic.py</code>. This will run moonshine model by default.
+For more details access READMEs in directories corresponding to models.
---
+++ transcription_from_mic.py
@@ -0,0 +1,23 @@
+import argparse
+import moonshine.live_transcription
+
+SUPPORTED_MODELS = ["moonshine"]
+MODEL = "moonshine"
+DEFAULT_MODEL_SIZES = {"moonshine":"moonshine/base"}
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        prog="transcription_from_mic",
+        description="Demo of transcription from default mic device"
+    )
+    parser.add_argument(
+        "-m", "--model",
+        help=f"Model to run. Default: moonshine.",
+        default="moonshine",
+        choices=SUPPORTED_MODELS
+    )
+    args = parser.parse_args()
+    model = args.model
+
+    if(model=='moonshine'):
+        moonshine.live_transcription.main(DEFAULT_MODEL_SIZES[model], include_captions=True)

849f98bd20e1f58b454c1062efbf981b367e51a5
dk729@cam.ac.uk <kvit2006@gmail.com>
2025-08-28 11:52:36 +0100

    Debugging messages

--- moonshine-enhanced/start.sh
+++ moonshine-enhanced/start.sh
@@ -4,9 +4,12 @@ DIR="$HOME/moonshine-enhanced"
 if [ ! -d "$DIR/.venv" ]; then
   uv venv
 fi
+echo "venv created"
 source "$DIR/.venv/bin/activate"
+echo "venv activated"
 if ! uv pip install -r requirements.txt --dry-run --no-index; then
     uv pip install -r requirements.txt
 fi
+echo "requirements installed; running demo"
 uv run "$DIR/main.py"
 

7070229e8a925c31ccd6e9a95a4c7d7ca335cdaa
dk729@cam.ac.uk <kvit2006@gmail.com>
2025-08-28 11:44:34 +0100

    Debugging

--- moonshine-enhanced/self_start_setup.sh
+++ moonshine-enhanced/self_start_setup.sh
@@ -2,7 +2,7 @@
 
 # Running this file makes the program run automatically after OS load
 
-if command -v uv &> /dev/null; then
+if ! command -v uv &> /dev/null; then
   curl -LsSf https://astral.sh/uv/install.sh | sh
 fi
 echo "./moonshine-enhanced/start.sh" >> .bashrc
--- moonshine-enhanced/start.sh
+++ moonshine-enhanced/start.sh
@@ -5,9 +5,8 @@ if [ ! -d "$DIR/.venv" ]; then
   uv venv
 fi
 source "$DIR/.venv/bin/activate"
-uv pip install -r requirements.txt --dry-run --no-index
-if [ $? -eq 1 ]; then
-  uv pip install -r requirements.txt
+if ! uv pip install -r requirements.txt --dry-run --no-index; then
+    uv pip install -r requirements.txt
 fi
 uv run "$DIR/main.py"
 

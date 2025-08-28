54c9edd4e54351fa2659c39547de33e4742613ce
dk729@cam.ac.uk <kvit2006@gmail.com>
2025-08-28 17:46:46 +0100

    imports fixed

--- moonshine-enhanced/start.sh
+++ moonshine-enhanced/start.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
 DIR="$HOME/moonshine-enhanced"
-if [ ! -d "$DIR/.venv" ]; then
+if [[ ! -d "$DIR/.venv" ]]; then
   uv venv "$DIR/.venv"
 fi
 source "$DIR/.venv/bin/activate"
-pkgs=$(grep -v "^#\|^$" "$DIR/requirements.txt" | sed 's/@.*//' | sed 's/[<>=].*//' | tr '\n' '|' | sed 's/|$//')
-if ! uv pip list --format=freeze | grep -q "$pkgs"; then
-  echo "Something not found. Installing requirements"
-  uv pip install -r "$DIR/requirements.txt"
+flag=uv pip install --dry-run --offline -r "$DIR/requirements.txt" | grep "Would make no changes"
+if uv pip install --dry-run --offline -r "$DIR/requirements.txt" | grep -q "Would make no changes"; then
+    uv pip install -r "$DIR/requirements.txt"
 fi
+MODEL_REQ="cat $DIR/model_requirements.txt"
+if ! uv pip list --format=freeze | grep "$MODEL_REQ"; then
+  uv pip install -r "$DIR/model_requirements.txt"
+fi
+
 uv run "$DIR/main.py"
 

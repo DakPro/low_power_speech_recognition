5d2998f978096a529a08f9f2fa7dd0e8b92715c8
dk729@cam.ac.uk <kvit2006@gmail.com>
2025-08-28 17:50:36 +0100

    fixing imports

--- moonshine-enhanced/start.sh
+++ moonshine-enhanced/start.sh
@@ -5,8 +5,8 @@ if [[ ! -d "$DIR/.venv" ]]; then
   uv venv "$DIR/.venv"
 fi
 source "$DIR/.venv/bin/activate"
-flag=uv pip install --dry-run --offline -r "$DIR/requirements.txt" | grep "Would make no changes"
-if uv pip install --dry-run --offline -r "$DIR/requirements.txt" | grep -q "Would make no changes"; then
+if ! uv pip install --dry-run --offline -r "$DIR/requirements.txt" | grep -q "Would make no changes"; then
+    echo "requirements missing"
     uv pip install -r "$DIR/requirements.txt"
 fi
 MODEL_REQ="cat $DIR/model_requirements.txt"

22f3c064b5bf08a947ac36801a7fdb6e3746a058
dk729@cam.ac.uk <kvit2006@gmail.com>
2025-08-28 17:18:15 +0100

    Update start.sh

--- moonshine-enhanced/start.sh
+++ moonshine-enhanced/start.sh
@@ -6,6 +6,9 @@ if [ ! -d "$DIR/.venv" ]; then
 fi
 source "$DIR/.venv/bin/activate"
 pkgs=$(grep -v "^#\|^$" "$DIR/requirements.txt" | sed 's/@.*//' | sed 's/[<>=].*//' | tr '\n' '|' | sed 's/|$//')
-uv pip list --format=freeze | grep -q "$pkgs" || { uv pip install -r "$DIR/requirements.txt"; }
+if ! uv pip list --format=freeze | grep -q "$pkgs"; then
+  echo "Something not found. Installing requirements"
+  uv pip install -r "$DIR/requirements.txt"
+fi
 uv run "$DIR/main.py"
 
